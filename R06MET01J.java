f5a522eff52f8c10681d38810303af8196e1a609
ottojmanducf <73440551+ottojmanducf@users.noreply.github.com>
2020-10-25 16:27:30 -0400

    Update R06MET01J.java

--- R06MET01J.java
+++ R06MET01J.java
@@ -9,3 +9,23 @@ public static int getAbsAdd(int x, int y) {
   assert (absX <= Integer.MAX_VALUE - absY);
   return absX + absY;
 }
+
+// ---------------------------------
+// v1.01 used assertions to check the valididty of the arguments. this can result in run-time errors
+// ---------------------------------
+
+// version 1.02
+// this version does not use assertions and validates by ensuring the MIN.VALUE isn't passed to Math.abs().
+// link: https://wiki.sei.cmu.edu/confluence/display/java/MET01-J.+Never+use+assertions+to+validate+method+arguments
+
+public static int getAbsAdd(int x, int y) {
+  if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+    throw new IllegalArgumentException();
+  }
+  int absX = Math.abs(x);
+  int absY = Math.abs(y);
+  if (absX > Integer.MAX_VALUE - absY) {
+    throw new IllegalArgumentException();
+  }
+  return absX + absY;
+}
