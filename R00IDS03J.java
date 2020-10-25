a61b8b4eb051578bd3ad40627c0da42458d8c931
ottojmanducf <73440551+ottojmanducf@users.noreply.github.com>
2020-10-25 15:58:41 -0400

    Create R00IDS03J.java

---
+++ R00IDS03J.java
@@ -0,0 +1,10 @@
+// version 1.01
+
+if (loginSuccessful) {
+  logger.severe("User login succeeded for: " + username);
+} else {
+  logger.severe("User login failed for: " + username);
+}
+
+// ------------------------------------------------------
+
