00e0fd699f11904d8afdb269c00f00e0ce0c1bae
ottojmanducf <73440551+ottojmanducf@users.noreply.github.com>
2020-10-25 16:50:48 -0400

    Update R10_THI01_J.java

--- R10_THI01_J.java
+++ R10_THI01_J.java
@@ -55,3 +55,36 @@ public final class NetworkHandler implements Runnable {
     tg.destroy();
   }
 }
+
+
+// ------------------------------
+// v1.01 had a NetworkHandler which mainted a controller thread. A lot of methods within ThreadGroup have been deprecated.
+// ------------------------------
+
+// version 1.02
+// the following code utilizes a fixed thread pool instead of a Group to group the tasks.
+// link: https://wiki.sei.cmu.edu/confluence/display/java/THI01-J.+Do+not+invoke+ThreadGroup+methods
+
+public final class NetworkHandler {
+  private final ExecutorService executor;
+ 
+  NetworkHandler(int poolSize) {
+    this.executor = Executors.newFixedThreadPool(poolSize);
+  }
+ 
+  public void startThreads() {
+    for (int i = 0; i < 3; i++) {
+      executor.execute(new HandleRequest());
+    }
+  }
+ 
+  public void shutdownPool() {
+    executor.shutdown();
+  }
+ 
+  public static void main(String[] args)  {
+    NetworkHandler nh = new NetworkHandler(3);
+    nh.startThreads();
+    nh.shutdownPool();
+  }
+}
