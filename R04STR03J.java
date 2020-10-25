dff53f64efc5b1218255f673d617b19badd785f7
ottojmanducf <73440551+ottojmanducf@users.noreply.github.com>
2020-10-25 16:21:27 -0400

    Create R04STR03J.java

---
+++ R04STR03J.java
@@ -0,0 +1,7 @@
+// version 1.01
+
+BigInteger x = new BigInteger("530500452766");
+byte[] byteArray = x.toByteArray();
+String s = new String(byteArray);
+byteArray = s.getBytes();
+x = new BigInteger(byteArray);
