1960cdf8ce31e916de420efd9bd4929cb59eff96
ottojmanducf <73440551+ottojmanducf@users.noreply.github.com>
2020-10-25 17:03:02 -0400

    Update R14_SER09_J.java

--- R14_SER09_J.java
+++ R14_SER09_J.java
@@ -7,5 +7,18 @@ private void readObject(final ObjectInputStream stream)
 }
  
 public void overridableMethod() {
-  // ...
+  // do stuff
+}
+
+// --------------------------------------
+// v1.01 calls the overridable method from readObjet()
+// --------------------------------------
+
+// version 1.02
+// the below code removes the premature call of the method overridableMethod().
+// link: https://wiki.sei.cmu.edu/confluence/display/java/SER09-J.+Do+not+invoke+overridable+methods+from+the+readObject%28%29+method
+
+private void readObject(final ObjectInputStream stream)
+                        throws IOException, ClassNotFoundException {
+  stream.defaultReadObject();
 }
