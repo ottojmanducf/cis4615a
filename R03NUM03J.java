fac8bf0e2041a4d66a73869735d74d33c4283f54
ottojmanducf <73440551+ottojmanducf@users.noreply.github.com>
2020-10-25 16:17:46 -0400

    Update R03NUM03J.java

--- R03NUM03J.java
+++ R03NUM03J.java
@@ -3,3 +3,14 @@
 public static int getInteger(DataInputStream is) throws IOException {
   return is.readInt(); 
 }
+
+// ----------------------------------
+// v1.01 has a vulnerability where the value could be mis-read of misunderstood. 
+// ----------------------------------
+
+// version 1.02
+public static long getInteger(DataInputStream is) throws IOException {
+  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
+}
+
+// link: https://wiki.sei.cmu.edu/confluence/display/java/NUM03-J.+Use+integer+types+that+can+fully+represent+the+possible+range+of++unsigned+data
